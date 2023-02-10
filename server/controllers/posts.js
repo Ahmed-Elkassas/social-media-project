45701905319611ac1835726954f25564b43e56fe
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2023-02-10 15:25:39 +0200

    Create Post req and it's route

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,11 +1,23 @@
 import PostMessage from "../models/postMsg.js";
 
-
 export const getPosts = async (req, res) => {
-    try {
-        const postMessage = await PostMessage.find();
-        res.status(200).json(postMessage);
-    } catch (error) {
-        res.status(404).json({message: error.message});
-    }
+  try {
+    const postMessage = await PostMessage.find();
+    res.status(200).json(postMessage);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+    const post = body.req;
+    const newPostMessage = new PostMessage(post);
+
+  try {
+    await newPostMessage.save();
+    res.status(201).json(newPostMessage);
+
+  } catch (error) {
+    res.status(409).json({message: error.message})
+  }
 };
