8defbf688c6e72f1a73259b6e50bce0eefe067dd
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2023-02-10 15:20:36 +0200

    Make response with getPosts req

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,11 @@
-export const getPosts = (req, res, next) => {
-    res.send(`It's working`)
+import PostMessage from "../models/postMsg";
+
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessage = await PostMessage.find();
+        res.status(200).json(postMessage);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
 };
