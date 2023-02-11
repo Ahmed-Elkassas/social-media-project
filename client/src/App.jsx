3f66a37f7235c1c27b3a91c75d11d9d28b2b9b3f
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2023-02-11 06:45:41 +0200

    Use redux-thunk to fetch posts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,51 @@
-import { AppBar, Container, Grid, Grow, Typography } from "@material-ui/core"
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
-import memories from './assets/images/memories.png';
+import { AppBar, Container, Grid, Grow, Typography } from "@material-ui/core";
 
-import useStyles from './appStyles';
+import { getPosts } from "./redux/actions/posts";
 import { Posts } from "./components/posts/Posts";
 import { Forms } from "./components/forms/Forms";
 
+import memories from "./assets/images/memories.png";
+import useStyles from "./appStyles";
+
 function App() {
   const classes = useStyles();
- 
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts);
+  }, []);
+
   return (
-   <Container maxWidth="lg">
-    <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-        <img  src={memories} className={classes.image} alt="icon" height="60" />
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} className={classes.image} alt="icon" height="60" />
       </AppBar>
       <Container>
         <Grow in>
-          <Grid container  justifyContent="space-between" alignItems="stretch" spacing={3}>
-            <Grid item  xs={12} sm={7}>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
               <Posts />
             </Grid>
-            <Grid item  xs={12} sm={4}>
+            <Grid item xs={12} sm={4}>
               <Forms />
             </Grid>
           </Grid>
         </Grow>
       </Container>
-   </Container>
-  )
+    </Container>
+  );
 }
 
-export default App
+export default App;
--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,6 +1,12 @@
+import { useSelector } from "react-redux"
 import { Post } from "./post/Post"
 
 export const Posts = () => {
+
+ const posts = useSelector(state => state.posts)
+
+  console.log(posts)
+
   return (
     <div>
         <Post />
