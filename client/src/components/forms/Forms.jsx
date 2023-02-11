6b443e2434d8f9d740bcbe3e303ab67c2d7cf1c6
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2023-02-11 09:00:37 +0200

    Collect form values

--- client/src/components/forms/Forms.jsx
+++ client/src/components/forms/Forms.jsx
@@ -1,13 +1,15 @@
 import { Button, Paper, TextField, Typography } from "@material-ui/core";
 
 import useStyles from './formStyles';
+import { useState } from "react";
 
 export const Forms = () => {
+  const classes = useStyles()
 
-   const classes = useStyles()
+  const [postData, setPostData] = useState({ title: '', message: '', tags: [], selectedFile: ''}) 
 
-  const submitHandler = (event) => {
-    event.preventDefault();
+  const submitHandler = (e) => {
+    e.preventDefault();
   };
 
   return (
@@ -20,7 +22,7 @@ export const Forms = () => {
           label="Creator"
           fullWidth
         />
-        <TextField name="title" variant="outlined" label="Title" fullWidth />
+        <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={() => setPostData({...postData, title: e.target.value})} />
         <TextField
           name="message"
           variant="outlined"
@@ -28,8 +30,9 @@ export const Forms = () => {
           fullWidth
           multiline
           maxRows={4}
+          value={postData.message} onChange={() => setPostData({...postData, message: e.target.value})}
         />
-        <TextField name="tags" variant="outlined" label="Tags" fullWidth />
+        <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={() => setPostData({...postData, tags: e.target.value})} />
         <Button type="submit" variant="contained" fullWidth color="primary">Submit</Button>
       </form>
     </Paper>
