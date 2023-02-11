d8483bf7db2e13aca39adf0919719d59a38e71c3
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2023-02-11 16:07:52 +0200

    Edit form values and test create new post

--- client/src/components/forms/Forms.jsx
+++ client/src/components/forms/Forms.jsx
@@ -4,16 +4,19 @@ import FileBase from 'react-file-base64';
 
 import useStyles from './formStyles';
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createPost } from "../../redux/actions/posts";
 
 export const Forms = () => {
   const classes = useStyles()
 
-  const [postData, setPostData] = useState({ title: '', message: '', tags: [], selectedFile: ''}) 
+  const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: [], selectedFile: ''}) 
+  const dispatch = useDispatch()
 
-  console.log(postData)
 
   const submitHandler = (e) => {
     e.preventDefault();
+    dispatch(createPost(postData))
   };
 
   return (
@@ -25,6 +28,7 @@ export const Forms = () => {
           variant="outlined"
           label="Creator"
           fullWidth
+          value={postData.creator} onChange={(e) => setPostData({...postData, creator: e.target.value})} 
         />
         <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({...postData, title: e.target.value})} />
         <TextField
@@ -36,7 +40,7 @@ export const Forms = () => {
           maxRows={4}
           value={postData.message} onChange={(e) => setPostData({...postData, message: e.target.value})}
         />
-        <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({...postData, tags: e.target.value})} />
+        <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({...postData, tags: e.target.value.split(',')})} />
         <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
         <Button type="submit" variant="contained" fullWidth color="primary">Submit</Button>
       </form>
